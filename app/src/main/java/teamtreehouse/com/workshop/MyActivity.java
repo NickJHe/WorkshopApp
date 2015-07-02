0ed4d411cdd1b69b40a28c5e58dc55d174a5903d
Nickolas Jay He <nickolas.jay.he@gmail.com>
2015-07-02 09:46:14 +0800

    Initial commit

---
+++ app/src/main/java/teamtreehouse/com/workshop/MyActivity.java
@@ -0,0 +1,55 @@
+package teamtreehouse.com.workshop;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+/*
+
+        Button button=(Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MyActivity.this,getString(R.string.special_text),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Treehouse temp=new Treehouse();
+
+
+        ParseObject object;
+*/
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/teamtreehouse/com/workshop/Treehouse.java
@@ -0,0 +1,8 @@
+package teamtreehouse.com.workshop;
+
+/**
+ * Created by Nickolas Jay He on 7/2/2015.
+ */
+public class Treehouse {
+
+}
